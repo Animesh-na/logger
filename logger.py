763df1eb4d48f736ce8a9b91300fcc27db66f7af
Animesh Nama <55810400+Animesh-na@users.noreply.github.com>
2024-09-23 09:19:40 +0530

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,13 +1,20 @@
 import logging
- 
-# Create and configure logger
+from datetime import datetime
+
+# Clear any existing logging handlers
 for handler in logging.root.handlers[:]:
     logging.root.removeHandler(handler)
-logging.basicConfig(filename='logs/project_name_'+ (datetime.now().date().strftime("%Y_%m_%d")) + '.log',
-                    format="%(asctime)s [%(levelname)s] %(message)s",
-                    filemode='a',
-                    level=logging.INFO)
+
+# Configure logging settings
+logging.basicConfig(
+    filename='logs/project_name_' + datetime.now().date().strftime("%Y_%m_%d") + '.log',
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    filemode='a',  # 'a' means append mode, 'w' means write/overwrite mode
+    level=logging.INFO  # Log level set to INFO
+)
+
+# Create a logger object
 logger = logging.getLogger(__name__)
 
-
-logger.info(f"hello")
+# Log a sample message
+logger.info("hello")
